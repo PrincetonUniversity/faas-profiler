53e8d42ff214fe506f9ecba810d61add7c4de5bb
engshahrad <engshahrad@gmail.com>
2019-11-07 02:25:57 +0000

    added notes to the perf script

--- monitoring/Perf.sh
+++ monitoring/Perf.sh
@@ -1,3 +1,7 @@
-# This script uses the linux "perf" tool to gather certain information.
+# This script uses the Linux "perf" tool to gather certain information.
+
+# Notes:
+# 1. The events listed below are just some of the examples. You can find the full list of events by running `perf list`.
+# 2. It is usually recommended to go for fewer events to ensure low profiling overhead.
 
 sudo perf stat -a -e cpu-cycles,L1-dcache-loads,L1-dcache-load-misses,L1-icache-load-misses,dTLB-load-misses,dTLB-loads,iTLB-load-misses,iTLB-loads,branch-misses,context-switches,cpu-migrations,page-faults -I $2 -o perf-mon.out sleep $1
