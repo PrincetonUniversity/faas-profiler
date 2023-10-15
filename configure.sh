af69ecd26ff11b5d7609d66b59c8e5ea1ec7dbc9
engshahrad <engshahrad@users.noreply.github.com>
2023-10-15 06:04:59 +0000

    config script fix: check for pip and install if needed

--- configure.sh
+++ configure.sh
@@ -1,8 +1,31 @@
 # This script sets some universal parameters.
 # It should be run oly once.
 
-# Installing some dependencies if needed
+# Installing basic dependencies if needed
 sudo apt-get install -y moreutils
+
+# Check if python3.10 is installed
+if ! command -v python3.10 &> /dev/null
+then
+    echo "Python 3.10 could not be found. Installing..."
+    sudo apt-get install -y software-properties-common
+    sudo add-apt-repository -y ppa:deadsnakes/ppa
+    sudo apt-get update
+    sudo apt-get install -y python3.10
+else
+    echo "Python 3.10 is already installed."
+fi
+
+# Check if pip is installed
+if ! command -v pip3 &> /dev/null
+then
+    echo "Pip could not be found. Installing..."
+    sudo apt-get install -y python3-pip
+else
+    echo "Pip is already installed."
+fi
+
+# Install python dependencies
 python3.10 -m pip install -r requirements.txt
 
 # Configure path variables used by the platform 
